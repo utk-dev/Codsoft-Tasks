a1f17d9ebe5a9a09273c05821cec73e1c3b876bb
Utkersh Pandyaa <utkershpandyaa@gmail.com>
2026-05-24 23:49:51 +0530

    Add files via upload

---
+++ 02_Task.py
@@ -0,0 +1,32 @@
+# CALCULATOR
+
+
+def calculator():
+    first_number = float(input("Enter the first number: "))
+    second_number = float(input("Enter the second number: "))
+    operation = input("Enter an operation(+,-,/,*,%): ")
+
+    if operation == "+":
+        result = first_number + second_number
+
+    elif operation == "-":
+        result = first_number - second_number
+
+    elif operation == "/":
+        if second_number == 0:
+            print("Division by 0 is not possible")
+            return
+        result = first_number / second_number
+    
+    elif operation == "*":
+        result = first_number * second_number
+    
+    elif operation == "%":
+        result = first_number % second_number
+    
+    else:
+        print("Operation Not Valid")
+    
+    print(f"The value of {first_number} {operation} {second_number} is {result:.4f}")      #answers upto 4 decimal places
+
+calculator()
