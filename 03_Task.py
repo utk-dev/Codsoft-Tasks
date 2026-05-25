38af8bcf07b3401a6a0d2c92a8018cba158281a7
Utkersh Pandyaa <utkershpandyaa@gmail.com>
2026-05-25 23:23:19 +0530

    Add files via upload

---
+++ 03_Task.py
@@ -0,0 +1,35 @@
+# Rock-Paper-Scissors Game
+
+'''
+Rock = -1
+Paper = 1
+Scissors = 0
+'''
+
+import random
+
+while True:
+    computer = random.choice([1,0,-1])
+
+    print(f"r for 🪨\np for 📄\ns for ✂️\nType stop for ending the game")
+
+    your_move = input("Enter your choice: ").lower()
+
+    if your_move == "stop":
+        print("GAME ENDED")
+        break
+
+    yourDict = {"r":-1, "p":1, "s":0}
+
+    you = yourDict[your_move]
+
+    reverseDict = {-1:"Rock", 1:"Paper", 0:"Scissors"}
+
+    print(f"Computer chose: {reverseDict[computer]} \nYou chose: {reverseDict[you]}")
+
+    if you == computer:
+        print("It's a Tie !!")
+        if (you-computer == -1) or (computer-you == 2):
+            print("Computer Wins!!")
+        else:
+            print("You win!!")
